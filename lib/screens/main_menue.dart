946b933d1a5534c97b63f49f875839a62e6def9e
Stewart Benyamin <Stewart.benyamin@student.fh-kiel.de>
2025-07-09 05:36:29 +0200

    implemented first screens

---
+++ lib/screens/main_menue.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainMenueScreen extends StatelessWidget {
+  const MainMenueScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(crossAxisAlignment: ,));
+  }
+}
